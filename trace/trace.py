de1f1f0b6092495e37feaccd87844fe793021ac9
yashpatel5400 <yashpatel5400@gmail.com>
2016-11-24 18:20:56 -0500

    Implemented U-Net -- may have bugs currently (or not run)

--- trace/trace.py
+++ trace/trace.py
@@ -66,7 +66,7 @@ def create_network(inpt, out, learning_rate=0.001):
         h_conv1 = tf.nn.relu(conv2d(image, W_conv1, dilation=1) + b_conv1)
 
         # layer 2   - 3x3 convolution -- SAVED
-        W_conv2 = weight_variable([3, 3, 1, 48])
+        W_conv2 = weight_variable([3, 3, 48, 48])
         b_conv2 = bias_variable([48])
         # use cropped output from convolution
         h_conv2 = tf.nn.relu(conv2d(h_conv1, W_conv2, dilation=1) + b_conv2)
@@ -75,13 +75,13 @@ def create_network(inpt, out, learning_rate=0.001):
         h_conv3 = max_pool(h_conv2, strides=[1,1], dilation=1)
 
         # layer 4   - 3x3 convolution  
-        W_conv4 = weight_variable([3, 3, 1, 48])
-        b_conv4 = bias_variable([48])
+        W_conv4 = weight_variable([3, 3, 48, 96])
+        b_conv4 = bias_variable([96])
         h_conv4 = tf.nn.relu(conv2d(h_conv3, W_conv4, dilation=2) + b_conv4)
 
         # layer 5   - 3x3 convolution  
-        W_conv5 = weight_variable([3, 3, 1, 48])
-        b_conv5 = bias_variable([48])
+        W_conv5 = weight_variable([3, 3, 96, 96])
+        b_conv5 = bias_variable([96])
         # use cropped output from convolution
         h_conv5 = tf.nn.relu(conv2d(h_conv4, W_conv5, dilation=2) + b_conv5)
 
@@ -89,13 +89,13 @@ def create_network(inpt, out, learning_rate=0.001):
         h_conv6 = max_pool(h_conv5, strides=[1,1], dilation=2)
 
         # layer 7   - 3x3 convolution  
-        W_conv7 = weight_variable([3, 3, 1, 48])
-        b_conv7 = bias_variable([48])
+        W_conv7 = weight_variable([3, 3, 96, 192])
+        b_conv7 = bias_variable([192])
         h_conv7 = tf.nn.relu(conv2d(h_conv6, W_conv7, dilation=4) + b_conv7)
 
         # layer 8   - 3x3 convolution  
-        W_conv8 = weight_variable([3, 3, 48, 48])
-        b_conv8 = bias_variable([48])
+        W_conv8 = weight_variable([3, 3, 192, 192])
+        b_conv8 = bias_variable([192])
         # use cropped output from convolution
         h_conv8 = tf.nn.relu(conv2d(h_conv7, W_conv8, dilation=4) + b_conv8)  
 
@@ -103,13 +103,13 @@ def create_network(inpt, out, learning_rate=0.001):
         h_conv9 = max_pool(h_conv8, strides=[1,1], dilation=4)
 
         # layer 10  - 3x3 convolution  
-        W_conv10 = weight_variable([3, 3, 48, 200])
-        b_conv10 = bias_variable([200])
+        W_conv10 = weight_variable([3, 3, 192, 384])
+        b_conv10 = bias_variable([384])
         h_conv10 = tf.nn.relu(conv2d(h_conv9, W_conv10, dilation=8  ) + b_conv10)
 
         # layer 11  - 3x3 convolution  
-        W_conv11 = weight_variable([3, 3, 200, 200])
-        b_conv11 = bias_variable([200])
+        W_conv11 = weight_variable([3, 3, 384, 384])
+        b_conv11 = bias_variable([384])
         # use cropped output from convolution
         h_conv11 = tf.nn.relu(conv2d(h_conv10, W_conv11, dilation=8 ) + b_conv11)
 
@@ -117,87 +117,87 @@ def create_network(inpt, out, learning_rate=0.001):
         h_conv12 = max_pool(h_conv11, strides=[1,1], dilation=8 )
 
         # layer 13  - 3x3 convolution  
-        W_conv13 = weight_variable([3, 3, 200, 400])
-        b_conv13 = bias_variable([400])
+        W_conv13 = weight_variable([3, 3, 384, 768])
+        b_conv13 = bias_variable([768])
         h_conv13 = tf.nn.relu(conv2d(h_conv12, W_conv13, dilation=16) + b_conv13)
 
         # layer 14  - 3x3 convolution  
-        W_conv14 = weight_variable([3, 3, 400, 400])
-        b_conv14 = bias_variable([400])
+        W_conv14 = weight_variable([3, 3, 768, 768])
+        b_conv14 = bias_variable([768])
         h_conv14 = tf.nn.relu(conv2d(h_conv13, W_conv14, dilation=16) + b_conv14)
 
         # ----------------------------------------------------------------------------------------
         # - Upsample and reconstruction phase                                                    -
         # ----------------------------------------------------------------------------------------
         # layer 15   - up-sample 2x2 convolution  
-        W_conv15 = weight_variable([2, 2, 400, 400])
-        b_conv15 = bias_variable([400])
-        h_conv15 = tf.nn.relu(deconv2d(h_conv14, W_conv15, [400, 400]) + b_conv15)
+        W_conv15 = weight_variable([2, 2, 768, 768])
+        b_conv15 = bias_variable([768])
+        h_conv15 = tf.nn.relu(deconv2d(h_conv14, W_conv15, [768, 768]) + b_conv15)
 
         # layer 15.5 - combine upsampling with Step 11.5
         comb_conv15 = crop_and_concat(h_conv11, h_conv15, [16])
 
         # layer 16   - 3x3 convolution  
-        W_conv16 = weight_variable([3, 3, 400, 200])
-        b_conv16 = bias_variable([200])
+        W_conv16 = weight_variable([3, 3, 768, 384])
+        b_conv16 = bias_variable([384])
         h_conv16 = tf.nn.relu(conv2d(comb_conv15, W_conv16, dilation=8) + b_conv16)
 
         # layer 17  - 3x3 convolution  
-        W_conv17 = weight_variable([3, 3, 200, 200])
-        b_conv17 = bias_variable([200])
+        W_conv17 = weight_variable([3, 3, 384, 384])
+        b_conv17 = bias_variable([384])
         h_conv17 = tf.nn.relu(conv2d(h_conv16, W_conv17, dilation=8) + b_conv17)
         
         # layer 18   - up-sample 2x2 convolution  
-        W_conv18 = weight_variable([2, 2, 200, 200])
-        b_conv18 = bias_variable([200])
+        W_conv18 = weight_variable([2, 2, 384, 384])
+        b_conv18 = bias_variable([384])
         h_conv18 = tf.nn.relu(deconv2d(h_conv17, W_conv18) + b_conv18)
 
         # layer 18.5 - combine upsampling with Step 8.5
         comb_conv18 = crop_and_concat(h_conv8, h_conv18, [32])
 
         # layer 19   - 3x3 convolution  
-        W_conv19 = weight_variable([3, 3, 400, 200])
-        b_conv19 = bias_variable([200])
+        W_conv19 = weight_variable([3, 3, 384, 192])
+        b_conv19 = bias_variable([192])
         h_conv19 = tf.nn.relu(conv2d(comb_conv18, W_conv19, dilation=4) + b_conv19)
 
         # layer 20  - 3x3 convolution  
-        W_conv20 = weight_variable([3, 3, 200, 200])
-        b_conv20 = bias_variable([200])
+        W_conv20 = weight_variable([3, 3, 192, 192])
+        b_conv20 = bias_variable([192])
         h_conv20 = tf.nn.relu(conv2d(h_conv19, W_conv20, dilation=4) + b_conv20)
         
         # layer 21   - up-sample 2x2 convolution  
-        W_conv21 = weight_variable([2, 2, 200, 200])
-        b_conv21 = bias_variable([200])
+        W_conv21 = weight_variable([2, 2, 192, 192])
+        b_conv21 = bias_variable([192])
         h_conv21 = tf.nn.relu(deconv2d(h_conv20, W_conv21) + b_conv21)
 
         # layer 21.5 - combine upsampling with Step 5.5
         comb_conv21 = crop_and_concat(h_conv5, h_conv21, [64])
 
         # layer 22   - 3x3 convolution  
-        W_conv22 = weight_variable([3, 3, 200, 48])
-        b_conv22 = bias_variable([48])
+        W_conv22 = weight_variable([3, 3, 192, 96])
+        b_conv22 = bias_variable([96])
         h_conv22 = tf.nn.relu(conv2d(comb_conv21, W_conv22, dilation=2) + b_conv22)
 
         # layer 23  - 3x3 convolution  
-        W_conv23 = weight_variable([3, 3, 48, 48])
-        b_conv23 = bias_variable([48])
+        W_conv23 = weight_variable([3, 3, 96, 96])
+        b_conv23 = bias_variable([96])
         h_conv23 = tf.nn.relu(conv2d(h_conv22, W_conv23, dilation=2) + b_conv23)
         
         # layer 24   - up-sample 2x2 convolution  
-        W_conv24 = weight_variable([2, 2, 48, 48])
-        b_conv24 = bias_variable([48])
+        W_conv24 = weight_variable([2, 2, 96, 96])
+        b_conv24 = bias_variable([96])
         h_conv24 = tf.nn.relu(deconv2d(h_conv23, W_conv24) + b_conv24)
 
         # layer 24.5 - combine upsampling with Step 2.5
         comb_conv24 = crop_and_concat(h_conv2, h_conv24, [128])
 
         # layer 25   - 3x3 convolution  
-        W_conv25 = weight_variable([3, 3, 48, 24])
-        b_conv25 = bias_variable([24])
+        W_conv25 = weight_variable([3, 3, 96, 48])
+        b_conv25 = bias_variable([48])
         h_conv25 = tf.nn.relu(conv2d(comb_conv24, W_conv25, dilation=1) + b_conv25)
 
         # layer 26  - 3x3 convolution  
-        W_conv26 = weight_variable([3, 3, 24, 24])
+        W_conv26 = weight_variable([3, 3, 48, 24])
         b_conv26 = bias_variable([24])
         h_conv26 = tf.nn.relu(conv2d(h_conv25, W_conv26, dilation=1) + b_conv26)
 
