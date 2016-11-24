568e3d3a05aef009addf39e4fc8b6ed24c593f89
yashpatel5400 <yashpatel5400@gmail.com>
2016-11-24 13:50:53 -0500

    test

--- trace/trace.py
+++ trace/trace.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# https://papers.nips.cc/paper/4741-deep-neural-networks-segment-neuronal-membranes-in-electron-microscopy-images.pdf
+# hsddfasdfadsf ttps://papers.nips.cc/paper/4741-deep-neural-networks-segment-neuronal-membranes-in-electron-microscopy-images.pdf
 from __future__ import print_function
 from __future__ import division
 
