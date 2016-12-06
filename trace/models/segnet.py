70804926fd85c49af1fc4ced1ef73a49070219fc
yashpatel5400 <yashpatel5400@gmail.com>
2016-12-05 23:09:56 -0500

    customized shape layers

--- trace/models/segnet.py
+++ trace/models/segnet.py
@@ -8,11 +8,31 @@ LARGE_LAYER = 3
 # taken from https://arxiv.org/pdf/1511.00561.pdf
 def default_SegNet():
     params = {
-        'm1': 48,
-        'm2': 48,
-        'm3': 48,
-        'm4': 48,
-        'm5': 48,
+        's1': [
+            [4, 4, 1, 4],
+            [4, 4, 4, 4],
+            [4, 4, 4, 4]
+        ],
+        's2': [
+            [4, 4, 4, 8],
+            [4, 4, 8, 8],
+            [4, 4, 8, 8]
+        ],
+        's3': [
+            [4, 4, 8, 16],
+            [4, 4, 16, 16],
+            [4, 4, 16, 16]
+        ],
+        's4': [
+            [4, 4, 16, 32],
+            [4, 4, 32, 32],
+            [4, 4, 32, 32]
+        ],
+        's5': [
+            [4, 4, 32, 64],
+            [4, 4, 64, 64],
+            [4, 4, 64, 64]
+        ],
 
         'fc': 200,
         'lr': 0.001,
@@ -20,16 +40,31 @@ def default_SegNet():
     }
     return SegNet(params)
 
+# convolution, batch normalization, and ReLU layer
+def conv_norm_relu(inlayer, shape, dilation):
+    # convolution
+    W_conv  = weight_variable(shape)
+    s_conv  = conv2d(inlayer, W_conv, dilation=dilation)
+
+    # batch normalization
+    mean_bn, var_bn = tf.nn.moments(s_conv, [0])
+    offset_bn = tf.Variable(tf.zeros([shape[-1]]))
+    scale_bn = tf.Variable(tf.ones([shape[-1]]))
+    bn_conv = tf.nn.batch_normalization(s_conv, mean=mean_bn, variance=var_bn, 
+        offset=offset_bn, scale=scale_bn, variance_epsilon=.0005)
+
+    # ReLU
+    return tf.nn.relu(bn_conv)
 
 class SegNet:
     def __init__(self, params):
 
         # Hyperparameters
-        m1 = params['m1']
-        m2 = params['m2']
-        m3 = params['m3']
-        m4 = params['m4']
-        m5 = params['m5']
+        s1 = params['s1']
+        s2 = params['s2']
+        s3 = params['s3']
+        s4 = params['s4']
+        s5 = params['s5']
 
         fc = params['fc']
         learning_rate = params['lr']
@@ -45,50 +80,50 @@ class SegNet:
 
         # layer set 1: 2 Conv/Batch/ReLU and 1 pool
         down_conv1 = self._downsample_layer(inlayer=self.image, 
-            shape=[4, 4, 1, m1], dilation=1, cbr_layers=2)
+            shape=s1, dilation=1, cbr_layers=2)
 
         # layer set 2: 2 Conv/Batch/ReLU and 1 pool
         down_conv2 = self._downsample_layer(inlayer=down_conv1, 
-            shape=[4, 4, m1, m2], dilation=2, cbr_layers=2)
+            shape=s2, dilation=2, cbr_layers=2)
 
         # layer set 3: 3 Conv/Batch/ReLU and 1 pool
         down_conv3 = self._downsample_layer(inlayer=down_conv2, 
-            shape=[4, 4, m2, m3], dilation=4, cbr_layers=3)
+            shape=s3, dilation=4, cbr_layers=3)
 
         # layer set 4: 3 Conv/Batch/ReLU and 1 pool
         down_conv4 = self._downsample_layer(inlayer=down_conv3, 
-            shape=[4, 4, m3, m4], dilation=8, cbr_layers=3)
+            shape=s4, dilation=8, cbr_layers=3)
 
         # layer set 5: 3 Conv/Batch/ReLU and 1 pool
         down_conv5 = self._downsample_layer(inlayer=down_conv4, 
-            shape=[4, 4, m4, m5], dilation=16, cbr_layers=3)
+            shape=s5, dilation=16, cbr_layers=3)
 
         # layer set 6: 1 unpool (upsampling) and 3 Conv/Batch/ReLU
         up_conv1 = self._upsample_layer(inlayer=down_conv5, 
-            shape=[4, 4, m5, m4], dilation=16, cbr_layers=3)
+            shape=s5, dilation=16, cbr_layers=3)
 
         # layer set 7: 1 unpool (upsampling) and 3 Conv/Batch/ReLU
         up_conv2 = self._upsample_layer(inlayer=up_conv1, 
-            shape=[4, 4, m4, m3], dilation=8, cbr_layers=3)
+            shape=s4, dilation=8, cbr_layers=3)
 
         # layer set 8: 1 unpool (upsampling) and 3 Conv/Batch/ReLU
         up_conv3 = self._upsample_layer(inlayer=up_conv2, 
-            shape=[4, 4, m3, m2], dilation=4, cbr_layers=3)
+            shape=s3, dilation=4, cbr_layers=3)
         
         # layer set 9: 1 unpool (upsampling) and 2 Conv/Batch/ReLU
         up_conv4 = self._upsample_layer(inlayer=up_conv3, 
-            shape=[4, 4, m2, m1], dilation=2, cbr_layers=2)
+            shape=s2, dilation=2, cbr_layers=2)
 
         # layer set 10: 1 unpool (upsampling) and 2 Conv/Batch/ReLU
         self.prediction = self._upsample_layer(inlayer=up_conv4, 
-            shape=[4, 4, m1, 2], dilation=1, cbr_layers=2)
+            shape=s1, dilation=1, cbr_layers=2)
 
-        self.sigmoid_prediction = tf.nn.softmax(self.prediction)
-        self.cross_entropy = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(self.prediction, self.target))
+        self.softmax = tf.nn.softmax(self.prediction)
+        self.cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(self.prediction, self.target))
         self.loss_summary = tf.scalar_summary('cross_entropy', self.cross_entropy)
-        self.train_step = tf.train.AdamOptimizer(learning_rate).minimize(self.cross_entropy)
+        self.train_step = tf.train.AdagradDAOptimizer(learning_rate).minimize(self.cross_entropy)
 
-        self.binary_prediction = tf.round(self.sigmoid_prediction)
+        self.binary_prediction = tf.round(self.softmax)
         self.pixel_error = tf.reduce_mean(tf.cast(tf.abs(self.binary_prediction - self.target), tf.float32))
         self.pixel_error_summary = tf.summary.scalar('pixel_error', self.pixel_error)
         self.validation_pixel_error_summary = tf.summary.scalar('validation pixel_error', self.pixel_error)
@@ -125,32 +160,28 @@ class SegNet:
         self.model_name = str.format('out-{}_lr-{}_m1-{}_m2-{}_m3-{}_m4-{}_fc-{}', self.out, learning_rate, map_1,
                                      map_2, map_3, map_4, fc)
 
-    def _downsample_layer(self, inlayer, shape=[4, 4, 1, 48], 
-        dilation=1, cbr_layers=2):
-
-        h_conv1 = conv_norm_relu(inlayer=inlayer, shape=shape, dilation=dilation)
-        h_conv2 = conv_norm_relu(inlayer=h_conv1, shape=shape, dilation=dilation)
+    def _downsample_layer(self, inlayer, shapes, dilation=1, cbr_layers=2):
+        h_conv1 = conv_norm_relu(inlayer=inlayer, shape=shapes[0], dilation=dilation)
+        h_conv2 = conv_norm_relu(inlayer=h_conv1, shape=shapes[1], dilation=dilation)
         
         if cbr_layers == SMALL_LAYER:
             return max_pool(h_conv2, strides=[1, 1], dilation=dilation)
         elif cbr_layers == LARGE_LAYER:
-            h_conv3 = conv_norm_relu(inlayer=h_conv2, shape=shape, dilation=dilation)
+            h_conv3 = conv_norm_relu(inlayer=h_conv2, shape=shapes[2], dilation=dilation)
             return max_pool(h_conv3, strides=[1, 1], dilation=dilation)
         else:
             raise ValueError('Illegal number of Conv/Batch/ReLU layers')
 
-    def _upsample_layer(self, inlayer, shape=[4, 4, 1, 48], 
-        dilation=1, cbr_layers=2):
-
+    def _upsample_layer(self, inlayer, shapes, dilation=1, cbr_layers=2):
         h_unpool = tf.image.resize_images(inlayer, shape, 
             method=tf.image.ResizeMethod.NEAREST_NEIGHBOR, align_corners=False)
-        h_conv1 = conv_norm_relu(inlayer=h_unpool, shape=shape, dilation=dilation)
-        h_conv2 = conv_norm_relu(inlayer=h_conv1, shape=shape, dilation=dilation)
+        h_conv1 = conv_norm_relu(inlayer=h_unpool, shape=shapes[0], dilation=dilation)
+        h_conv2 = conv_norm_relu(inlayer=h_conv1, shape=shapes[1], dilation=dilation)
 
         if cbr_layers == SMALL_LAYER:
             return h_conv2
         elif cbr_layers == LARGE_LAYER:
-            h_conv3 = conv_norm_relu(inlayer=h_conv2, shape=shape, dilation=dilation)
+            h_conv3 = conv_norm_relu(inlayer=h_conv2, shape=shapes[2], dilation=dilation)
             return h_conv3
         else:
             raise ValueError('Illegal number of Conv/Batch/ReLU layers')
