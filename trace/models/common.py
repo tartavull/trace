429e34cfb96879e152c89765a5faf7e963c0bae4
sharonyou <sharon.you2@gmail.com>
2017-01-31 16:08:25 -0500

    Update common.py

--- trace/models/common.py
+++ trace/models/common.py
@@ -26,3 +26,30 @@ def max_pool(x, dilation=None, strides=[2, 2], window_shape=[2, 2]):
                        strides=strides, padding='VALID', pooling_type='MAX')
 
 
+"""
+Following methods adapted/borrowed from https://github.com/jakeret/tf_unet
+"""
+
+def conv2d_u(x, W, keep_prob_):
+    conv_2d = tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='VALID')
+    return tf.nn.dropout(conv_2d, keep_prob_)
+
+
+def deconv2d_u(x, W,stride):
+    x_shape = tf.shape(x)
+    output_shape = tf.pack([x_shape[0], x_shape[1]*2, x_shape[2]*2, x_shape[3]//2])
+    return tf.nn.conv2d_transpose(x, W, output_shape, strides=[1, stride, stride, 1], padding='VALID')
+
+
+def max_pool_u(x):
+    return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='VALID')
+
+
+def crop_and_concat(x1,x2):
+    x1_shape = tf.shape(x1)
+    x2_shape = tf.shape(x2)
+    # offsets for the top left corner of the crop
+    offsets = [0, (x1_shape[1] - x2_shape[1]) // 2, (x1_shape[2] - x2_shape[2]) // 2, 0]
+    size = [-1, x2_shape[1], x2_shape[2], -1]
+    x1_crop = tf.slice(x1, offsets, size)
+    return tf.concat(3, [x1_crop, x2])
