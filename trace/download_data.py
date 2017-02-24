25de1d1aecfcfb57117f1494ee9a6cdc9aba7d7c
Nathan Lam <nlam@princeton.edu>
2017-02-24 15:52:37 -0500

    modified to save cremi as hdf

--- trace/download_data.py
+++ trace/download_data.py
@@ -25,11 +25,13 @@ TEST_AFFINITIES = 'test-affinities'
 ZIP = '.zip'
 TIF = '.tif'
 H5 = '.h5'
-HDF = '.hdf'
 
 SNEMI3D = 'snemi3d'
 ISBI = 'isbi'
 CREMI = 'cremi'
+CREMI_A = 'cremi/a/'
+CREMI_B = 'cremi/b/'
+CREMI_C = 'cremi/c/'
 
 DATASET_NAMES = [SNEMI3D, ISBI, CREMI]
 
@@ -51,17 +53,6 @@ def __maybe_create_hdf5_from_tif(folder, base_fn):
             with h5py.File(full_path) as f:
                 print('created ' + base_fn + H5)
                 f.create_dataset('main', data=arr)
-
-# TODO: Modify so that it works for non-cremi files
-def __maybe_create_tif_from_hdf5(folder, base_fn):
-    full_path = folder + base_fn + TIF
-     
-    if not os.path.exists(full_path):
-        print(folder + base_fn + HDF)
-        file = CremiFile(folder + base_fn + HDF, 'r')
-        arr = file.read_raw().data
-        print('created ' + base_fn + TIF)
-        tif.imsave(full_path, arr)
 
 def __maybe_create_hdf5_from_tif_batch(folder, suffix):
     __maybe_create_hdf5_from_tif(folder, TRAIN_INPUT + suffix)
@@ -107,13 +98,6 @@ def __maybe_split(folder,
 
                 tif.imsave(folder + train_fn, train_set)
                 tif.imsave(folder + val_fn, validation_set)
-
-def __maybe_split_with_suffix(folder, train_frac, suffix):
-    __maybe_split(folder, train_frac, 
-                input=TRAIN_INPUT + suffix, 
-                input_labels=TRAIN_LABELS + suffix, 
-                valid=VALIDATION_INPUT + suffix, 
-                valid_labels=VALIDATION_LABELS + suffix)
 
 def __maybe_create_isbi(dest_folder, train_frac):
     base_url = 'http://brainiac2.mit.edu/isbi_challenge/sites/default/files/'
@@ -171,49 +155,74 @@ def __maybe_create_snemi3d(dest_folder, train_frac):
     __maybe_create_hdf5_from_tif(dest_folder, VALIDATION_LABELS)
     __maybe_create_hdf5_from_tif(dest_folder, TEST_INPUT)
 
-def __maybe_extract_cremi_labels(folder, base_fn, source_fn):
-    full_path = folder + base_fn + TIF
-     
-    if not os.path.exists(full_path):
-        file = CremiFile(folder + source_fn + HDF, 'r')
-        arr = file.read_neuron_ids().data
-        print('created ' + base_fn + TIF)
-        tif.imsave(full_path, arr)
+def __maybe_split_cremi(folder, train_fraction):
+    if not os.path.exists(folder + 'validation.hdf'):
+        print(str.format('splitting {} into {}% training,  {}% into validation', folder + 'train-full.hdf',
+                         100 * train_fraction, 100 * (1 - train_fraction)))
+
+        # Extract the input and labels from the hdf
+        o_train_file = cremiio.CremiFile(folder + 'train-full.hdf', 'r')
+        o_input_volume = o_train_file.read_raw()
+        o_input = o_input_volume.data.value
+        o_input_res = o_input_volume.resolution
+
+        o_labels_volume = o_train_file.read_neuron_ids()
+        o_labels = o_labels_volume.data.value
+        o_labels_res = o_input_volume.resolution
+
+        o_train_file.close()
+
+        # Split
+        num_slices = o_input.shape[0]
+        train_slices = int(num_slices * train_fraction)
+
+        train_input = o_input[:train_slices, :, :]
+        validation_input = o_input[train_slices:, :, :]
+
+        train_labels = o_labels[:train_slices, :, :]
+        validation_labels = o_labels[train_slices:, :, :]
+
+        train_input_file = cremiio.CremiFile(folder + TRAIN_INPUT, 'w')
+        train_input_file.write_raw(cremiio.Volume(train_input, resolution=o_input_res))
+        train_input_file.close()
+
+        train_labels_file = cremiio.CremiFile(folder + TRAIN_LABELS, 'w')
+        train_labels_file.write_neuron_ids(cremiio.Volume(train_labels, resolution=o_labels_res))
+        train_labels_file.close()
+
+        validation_input_file = cremiio.CremiFile(folder + VALIDATION_INPUT, 'w')
+        validation_input_file.write_raw(cremiio.Volume(validation_input, resolution=o_input_res))
+        validation_input_file.close()
+
+        validation_labels_file = cremiio.CremiFile(folder + VALIDATION_LABELS, 'w')
+        validation_labels_file.write_neuron_ids(cremiio.Volume(validation_labels, resolution=o_labels_res))
+        validation_labels_file.close()
 
 def __maybe_create_cremi(dest_folder, train_frac):
     base_url = 'https://cremi.org/static/data/'
-    train_A_sc = 'sample_A_20160501.hdf'
-    train_B_sc = 'sample_B_20160501.hdf'
-    train_C_sc = 'sample_C_20160501.hdf'
-    test_A_sc = 'sample_A%2B_20160601.hdf'
-    test_B_sc = 'sample_B%2B_20160601.hdf'
-    test_C_sc = 'sample_C%2B_20160601.hdf'
 
-    __maybe_download(base_url, train_A_sc, dest_folder, TRAIN_INPUT + '_A' + HDF)
-    # __maybe_download(base_url, train_B_sc, dest_folder, TRAIN_INPUT + '_B' + HDF)
-    # __maybe_download(base_url, train_C_sc, dest_folder, TRAIN_INPUT + '_C' + HDF)
-    __maybe_download(base_url, test_A_sc, dest_folder, TEST_INPUT + '_A' + HDF)
-    # __maybe_download(base_url, test_B_sc, dest_folder, TEST_INPUT + '_B' + HDF)
-    # __maybe_download(base_url, test_C_sc, dest_folder, TEST_INPUT + '_C' + HDF)
+    # For now, only download the un-padded versions
+    a_train_fn = 'sample_A_20160501.hdf'
+    a_test_fn = 'sample_A%2B_20160601.hdf'
 
-    __maybe_create_tif_from_hdf5(dest_folder, TRAIN_INPUT + '_A')
-    # __maybe_create_tif_from_hdf5(dest_folder, TRAIN_INPUT + '_B')
-    # __maybe_create_tif_from_hdf5(dest_folder, TRAIN_INPUT + '_C')
-    __maybe_create_tif_from_hdf5(dest_folder, TEST_INPUT + '_A')
-    # __maybe_create_tif_from_hdf5(dest_folder, TEST_INPUT + '_B')
-    # __maybe_create_tif_from_hdf5(dest_folder, TEST_INPUT + '_C')
+    b_train_fn = 'sample_B_20160501.hdf'
+    b_test_fn = 'sample_B%2B_20160601.hdf'
 
-    __maybe_extract_cremi_labels(dest_folder, TRAIN_LABELS + '_A', TRAIN_INPUT + '_A')
-    # __maybe_extract_cremi_labels(dest_folder, TRAIN_LABELS + '_B', TRAIN_INPUT + '_B')
-    # __maybe_extract_cremi_labels(dest_folder, TRAIN_LABELS + '_C', TRAIN_INPUT + '_C')
+    c_train_fn = 'sample_C_20160501.hdf'
+    c_test_fn = 'sample_C%2B_20160601.hdf'
 
-    __maybe_split_with_suffix(dest_folder, train_frac, '_A')
-    # __maybe_split_with_suffix(dest_folder, train_frac, '_B')
-    # __maybe_split_with_suffix(dest_folder, train_frac, '_C')
+    __maybe_download(base_url, a_train_fn, dest_folder + 'a/', 'train-full.hdf')
+    __maybe_download(base_url, a_test_fn, dest_folder + 'a/', 'test.hdf')
 
-    # __maybe_create_hdf5_from_tif_batch(dest_folder, '_A')
-    # __maybe_create_hdf5_from_tif_batch(dest_folder, '_B')
-    # __maybe_create_hdf5_from_tif_batch(dest_folder, '_C')
+    __maybe_download(base_url, b_train_fn, dest_folder + 'b/', 'train-full.hdf')
+    __maybe_download(base_url, b_test_fn, dest_folder + 'b/', 'test.hdf')
+
+    __maybe_download(base_url, c_train_fn, dest_folder + 'c/', 'train-full.hdf')
+    __maybe_download(base_url, c_test_fn, dest_folder + 'c/', 'test.hdf')
+
+    __maybe_split_cremi(dest_folder + 'a/', train_frac)
+    __maybe_split_cremi(dest_folder + 'b/', train_frac)
+    __maybe_split_cremi(dest_folder + 'c/', train_frac)
 
 def maybe_create_all_datasets(trace_folder, train_frac):
     # __maybe_create_snemi3d(trace_folder + SNEMI3D + '/', train_frac)
