1ce97f348c8f513714030a9ce2acebe0c8a67d90
Nathan Lam <nlam@princeton.edu>
2017-03-29 02:59:50 -0400

    fixed maxpooling in z

--- trace/models/common.py
+++ trace/models/common.py
@@ -46,7 +46,7 @@ def down_conv2d(x, W, dilation=1):
 
 
 def down_conv3d(x, W, dilation=1, z_dilation=1):
-    return tf.nn.convolution(x, W, strides=[2, 2, 2], padding='SAME', dilation_rate=[z_dilation, dilation, dilation])
+    return tf.nn.convolution(x, W, strides=[1, 2, 2], padding='SAME', dilation_rate=[z_dilation, dilation, dilation])
 
 
 def conv2d_transpose(x, W, stride):
@@ -57,7 +57,7 @@ def conv2d_transpose(x, W, stride):
 
 def conv3d_transpose(x, W, stride):
     x_shape = tf.shape(x)
-    output_shape = tf.stack([x_shape[0], x_shape[1] * 2, x_shape[2] * 2, x_shape[3] * 2, x_shape[4] // 2])
+    output_shape = tf.stack([x_shape[0], x_shape[1], x_shape[2] * 2, x_shape[3] * 2, x_shape[4] // 2])
     return tf.nn.conv3d_transpose(x, W, output_shape, strides=[1, stride, stride, stride, 1], padding='SAME')
 
 
@@ -66,7 +66,7 @@ def max_pool(x, dilation=1, strides=[2, 2], window_shape=[2, 2]):
                       strides=strides, padding='VALID', pooling_type='MAX')
 
 
-def max_pool_3d(x, dilation=1, z_dilation=1, strides=[2, 2, 2], window_shape=[2, 2, 2]):
+def max_pool_3d(x, dilation=1, z_dilation=1, strides=[1, 2, 2], window_shape=[1, 2, 2]):
     return tf.nn.pool(x, window_shape=window_shape, dilation_rate=[z_dilation, dilation, dilation],
                       strides=strides, padding='VALID', pooling_type='MAX')
 
