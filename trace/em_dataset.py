e1ccf9e580ca9400804f390b3401bfb95145409c
Nathan Lam <nlam@princeton.edu>
2017-04-11 00:05:23 -0400

    added seg3d->bound

--- trace/em_dataset.py
+++ trace/em_dataset.py
@@ -346,8 +346,11 @@ class EMDatasetSampler(object):
             if label_output_type == AFFINITIES_3D:
                 deformed_labels = affinitize(deformed_labels)
             elif label_output_type == BOUNDARIES:
+                print(deformed_labels.shape)
                 deformed_labels = convert_between_label_types(dataset.name, dataset.label_type, label_output_type,
                             expand_3d_to_5d(deformed_labels))
+                print(deformed_labels.shape)
+                print(deformed_labels)
             # Crop the image, to remove the padding that was added to allow safe augmentation.
             cropped_inputs = leveled_inputs[:, z_crop_pad // 2:-(z_crop_pad // 2), crop_pad // 2:-(crop_pad // 2), crop_pad // 2:-(crop_pad // 2), :]
             cropped_labels = deformed_labels[:, z_crop_pad // 2:-(z_crop_pad // 2), crop_pad // 2:-(crop_pad // 2), crop_pad // 2:-(crop_pad // 2), :]
