3d1322624933f5b71857f888969352c24934f9a6
Nathan Lam <nlam@princeton.edu>
2017-04-27 23:14:51 -0400

    added false pos and false neg detection

--- trace/evaluation.py
+++ trace/evaluation.py
@@ -141,26 +141,24 @@ def cleft_stats(pred, truth):
 
     print(np.max(pred_file.read_clefts().data.value))
 
-    truth_vol = truth_file.read_raw()
-    truth_input = truth_vol.data.value
-    truth_res = truth_vol.resolution
+    # truth_vol = truth_file.read_raw()
+    # truth_input = truth_vol.data.value
+    # truth_res = truth_vol.resolution
 
-    truth_cleft_vol = truth_file.read_clefts()
-    truth_cleft = truth_cleft_vol.data.value
-    truth_cleft_res = truth_cleft_vol.resolution
+    # truth_cleft_vol = truth_file.read_clefts()
+    # truth_cleft = truth_cleft_vol.data.value
+    # truth_cleft_res = truth_cleft_vol.resolution
 
-    truth_cleft = truth_cleft[1:, 1:, 1:]
+    # temp_file = cremiio.CremiFile('temp_file.hdf', 'w')
+    # temp_file.write_raw(cremiio.Volume(truth_input, resolution=truth_res))
+    # temp_file.write_clefts(cremiio.Volume(truth_cleft, resolution=truth_cleft_res))
+    # temp_file.close()
 
-    temp_file = cremiio.CremiFile('temp_file.hdf', 'w')
-    temp_file.write_raw(cremiio.Volume(truth_input, resolution=truth_res))
-    temp_file.write_clefts(cremiio.Volume(truth_cleft, resolution=truth_cleft_res))
-    temp_file.close()
+    # temp_file = cremiio.CremiFile('temp_file.hdf', 'r')
 
-    temp_file = cremiio.CremiFile('temp_file.hdf', 'r')
+    clefts_eval = Clefts(pred_file.read_clefts(), truth_file.read_clefts())
 
-    clefts_eval = Clefts(pred_file.read_clefts(), temp_file.read_clefts())
-
-    temp_file.close()
+    # temp_file.close()
     pred_file.close()
     truth_file.close()
 
