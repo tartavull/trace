0bd067f12e351f1ba2dff981c56b26c8e60565ad
Nathan Lam <nlam@princeton.edu>
2017-04-29 18:58:13 -0400

    changed thresholding scheme for prediction because too few TPs too many FNs after 0.05 thresh

--- trace/em_dataset.py
+++ trace/em_dataset.py
@@ -220,9 +220,9 @@ class CREMIDataset(Dataset):
         input_file.close()
 
         scores = {}
-        min_thresh = 0.05
+        min_thresh = 0.02
         if self.task =='cleft':
-            for threshold in np.arange(0, 1, 0.05):
+            for threshold in np.arange(0, .6, 0.02):
                 trans_predictions = convert_label_for_cremi_cleft(predictions, threshold)
                 
                 # write temp file for prediction
